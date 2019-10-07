6d0a4c10bc5fcfe752655567ad1a43e1820a9033
Clément Hamada <clementhamada@protonmail.com>
2019-10-07 19:41:40 +0200

    Implement memmove

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -1,26 +1,29 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft.h                                             .::    .:/ .      .::   */
+/*   ft_memmove.c                                     .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/08/14 23:24:01 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/07 19:11:44 by chamada     ###    #+. /#+    ###.fr     */
+/*   Created: 2019/10/07 18:53:59 by chamada      #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/07 19:40:32 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#ifndef FT_H
-# define FT_H
+void	*ft_memmove(void *dst, const void *src, unsigned long len)
+{
+	unsigned long i;
 
-void	*ft_memset(void *b, int c, unsigned long len);
-void	ft_bzero(void *s, unsigned long n);
-void
-	*ft_memcpy(void *restrict dst, const void *restrict src, unsigned long n);
-void*
-	ft_memccpy(void *restrict dst, const void *restrict src,
-	int c, unsigned long n);
-void	*ft_memmove(void *dst, const void *src, unsigned long len);
-
-#endif
+	i = 0;
+	if (src < dst)
+		while (i < len)
+		{
+			((char*)dst)[i] = ((char*)src)[i];
+			i++;
+		}
+	else if (dst != src)
+		while (len--)
+			((char*)dst)[len] = ((char*)src)[len];
+	return (dst);
+}
