03853c35f398790d76c335a6d209bbfe12fc357f
Clément Hamada <clementhamada@protonmail.com>
2019-10-07 20:46:26 +0200

    Remove restrict from prototypes

--- includes/ft.h
+++ includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/14 23:24:01 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/07 20:36:11 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/07 20:45:58 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,11 +16,8 @@
 
 void	*ft_memset(void *b, int c, unsigned long len);
 void	ft_bzero(void *s, unsigned long n);
-void
-	*ft_memcpy(void *restrict dst, const void *restrict src, unsigned long n);
-void*
-	ft_memccpy(void *restrict dst, const void *restrict src,
-	int c, unsigned long n);
+void	*ft_memcpy(void *dst, const void *src, unsigned long n);
+void*	ft_memccpy(void *dst, const void *src, int c, unsigned long n);
 void	*ft_memmove(void *dst, const void *src, unsigned long len);
 void	*ft_memchr(const void *s, int c, unsigned long n);
 
