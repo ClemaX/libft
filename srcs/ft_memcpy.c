47c5fb1fce71ec011f6f5c8fd509450e72469306
Clément Hamada <clementhamada@protonmail.com>
2019-10-07 16:49:21 +0200

    Implemented memcpy

--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -1,22 +1,23 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft.h                                             .::    .:/ .      .::   */
+/*   ft_memcpy.c                                      .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/08/14 23:24:01 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/07 16:49:15 by chamada     ###    #+. /#+    ###.fr     */
+/*   Created: 2019/10/07 16:03:24 by chamada      #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/07 16:46:42 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#ifndef FT_H
-# define FT_H
-
-void	*ft_memset(void *b, int c, unsigned long len);
-void	ft_bzero(void *s, unsigned long n);
 void
-	*ft_memcpy(void *restrict dst, const void *restrict src, unsigned long n);
+	*ft_memcpy(void *restrict dst, const void *restrict src, unsigned long n)
+{
+	char *temp;
 
-#endif
+	temp = dst;
+	while (n--)
+		*(temp++) = *(char *)(src++);
+	return (dst);
+}
