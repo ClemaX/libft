c191a2202907e314d04de7fec8a5da745fccf995
Clément Hamada <clementhamada@protonmail.com>
2019-10-09 00:21:47 +0200

    Use bzero in calloc

--- srcs/ft_calloc.c
+++ srcs/ft_calloc.c
@@ -6,12 +6,13 @@
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 22:17:12 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/08 22:27:41 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/09 00:21:34 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "types.h"
+#include "ft.h"
 #include <stdlib.h>
 
 void	*ft_calloc(t_size count, t_size size)
@@ -21,7 +22,6 @@ void	*ft_calloc(t_size count, t_size size)
 	buf = malloc(count * size);
 	if (!buf)
 		return (NULL);
-	while (size--)
-		((char*)buf)[size] = 0;
+	ft_bzero(buf, size);
 	return (buf);
 }
