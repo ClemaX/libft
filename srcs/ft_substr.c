cd9a82e50086b22f06047adb26bc1a1b679c2056
Clément Hamada <clementhamada@protonmail.com>
2019-10-08 13:01:25 +0200

    Implement ft_substr

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -1,30 +1,32 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft.h                                             .::    .:/ .      .::   */
+/*   ft_substr.c                                      .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/08/14 23:24:01 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/08 12:41:06 by chamada     ###    #+. /#+    ###.fr     */
+/*   Created: 2019/10/08 12:39:22 by chamada      #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/08 12:54:50 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "types.h"
+#include <stdlib.h>
 
-#ifndef FT_H
-# define FT_H
+char	*ft_substr(char const *s, unsigned int start, t_size len)
+{
+	char	*substr;
+	t_size	i;
 
-void	*ft_memset(void *b, int c, t_size len);
-void	ft_bzero(void *s, t_size n);
-void	*ft_memcpy(void *dst, const void *src, t_size n);
-void*	ft_memccpy(void *dst, const void *src, int c, t_size n);
-void	*ft_memmove(void *dst, const void *src, t_size len);
-void	*ft_memchr(const void *s, int c, t_size n);
-int		ft_memcmp(const void *s1, const void *s2, t_size n);
-int		ft_strlen(const char *s);
-
-char	*ft_substr(char const *s, unsigned int start, t_size len);
-
-#endif
+	i = start;
+	if (!(substr = malloc(len + 1)))
+		return (NULL);
+	while (i - start < len)
+	{
+		substr[i - start] = s[i];
+		i++;
+	}
+	substr[len] = 0;
+	return (substr);
+}
