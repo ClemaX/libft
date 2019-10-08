b7938c44bf5eeb81b89f470b65d52ad86661df7c
Clément Hamada <clementhamada@protonmail.com>
2019-10-08 12:21:48 +0200

    Implement strlen

--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -1,26 +1,22 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft.h                                             .::    .:/ .      .::   */
+/*   ft_strlen.c                                      .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/08/14 23:24:01 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/08 12:20:59 by chamada     ###    #+. /#+    ###.fr     */
+/*   Created: 2019/10/08 12:17:23 by chamada      #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/08 12:21:08 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#ifndef FT_H
-# define FT_H
+int	ft_strlen(const char *s)
+{
+	int	len;
 
-void	*ft_memset(void *b, int c, unsigned long len);
-void	ft_bzero(void *s, unsigned long n);
-void	*ft_memcpy(void *dst, const void *src, unsigned long n);
-void*	ft_memccpy(void *dst, const void *src, int c, unsigned long n);
-void	*ft_memmove(void *dst, const void *src, unsigned long len);
-void	*ft_memchr(const void *s, int c, unsigned long n);
-int		ft_memcmp(const void *s1, const void *s2, unsigned long n);
-int		ft_strlen(const char *s);
-
-#endif
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
