f39a7b817ffb0caa595b683e297de6b3fa7ffee1
Clément Hamada <clementhamada@protonmail.com>
2019-10-08 21:49:28 +0200

    Implement ft_putchar_fd

--- includes/ft.h
+++ includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/14 23:24:01 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/08 21:33:32 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/08 21:47:49 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -32,5 +32,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_putchar_fd(char c, int fd);
 
 #endif
