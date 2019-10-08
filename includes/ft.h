b1477e141cd215a26a198e0c745483edf0bcadc0
Clément Hamada <clementhamada@protonmail.com>
2019-10-08 21:33:23 +0200

    strlen: Fix return type

--- includes/ft.h
+++ includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/14 23:24:01 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/08 21:00:26 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/08 21:32:42 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -23,7 +23,7 @@ void*	ft_memccpy(void *dst, const void *src, int c, t_size n);
 void	*ft_memmove(void *dst, const void *src, t_size len);
 void	*ft_memchr(const void *s, int c, t_size n);
 int		ft_memcmp(const void *s1, const void *s2, t_size n);
-int		ft_strlen(const char *s);
+t_size	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 
 char	*ft_substr(char const *s, unsigned int start, t_size len);
--- srcs/ft_strlen.c
+++ srcs/ft_strlen.c
@@ -6,14 +6,16 @@
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 12:17:23 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/08 12:21:08 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/08 21:25:00 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-int	ft_strlen(const char *s)
+#include "types.h"
+
+t_size	ft_strlen(const char *s)
 {
-	int	len;
+	t_size len;
 
 	len = 0;
 	while (s[len])
