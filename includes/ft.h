53e03ad9c41b7a2f84ec71d3082b69a5a231ed4d
Clément Hamada <clementhamada@protonmail.com>
2019-10-08 21:18:28 +0200

    Implement ft_itoa

--- includes/ft.h
+++ includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/14 23:24:01 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/08 14:48:57 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/08 21:00:26 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -30,5 +30,6 @@ char	*ft_substr(char const *s, unsigned int start, t_size len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
---
+++ srcs/ft_itoa.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_itoa.c                                        .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/08 20:37:08 by chamada      #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/08 21:12:59 by chamada     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static int	uintlen(unsigned int n)
+{
+	int len;
+
+	len = 1;
+	while (n /= 10)
+		len++;
+	return (len);
+}
+
+char		*ft_itoa(int n)
+{
+	unsigned int	u_n;
+	int				len;
+	char			sign;
+	char			*a;
+
+	sign = n < 0;
+	u_n = sign ? -n : n;
+	len = uintlen(u_n) + sign;
+	if (!(a = malloc(len + 1)))
+		return (NULL);
+	if (sign)
+		a[0] = '-';
+	a[len] = 0;
+	while (len-- - sign)
+	{
+		a[len] = u_n % 10 + '0';
+		u_n /= 10;
+	}
+	return (a);
+}
