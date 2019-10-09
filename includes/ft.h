cf6d0e9446586fbb09ac23adefd8609cf0ba3220
Clément Hamada <clementhamada@protonmail.com>
2019-10-09 19:05:45 +0200

    Implement toupper

--- includes/ft.h
+++ includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/14 23:24:01 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/09 18:37:41 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/09 19:03:17 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -41,6 +41,7 @@ int		ft_isspace(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_atoi(const char *str);
+int		ft_toupper(char c);
 
 char	*ft_substr(char const *s, unsigned int start, t_size len);
 char	*ft_strjoin(char const *s1, char const *s2);
