bb67f8c599b93873551d26bd668b4943560be2cf
Clément Hamada <clementhamada@protonmail.com>
2019-10-09 17:51:58 +0200

    Implement islower

--- includes/ft.h
+++ includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/14 23:24:01 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/09 17:48:40 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/09 17:51:42 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -33,6 +33,7 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *haystack, const char *needle, t_size len);
 int		ft_strncmp(const char *s1, const char *s2, t_size n);
 int		ft_isupper(int c);
+int		ft_islower(int c);
 
 char	*ft_substr(char const *s, unsigned int start, t_size len);
 char	*ft_strjoin(char const *s1, char const *s2);
