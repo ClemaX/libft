5364a937f24da3550a4d21df95c4a57f429e9126
Clément Hamada <clementhamada@protonmail.com>
2019-10-09 18:02:10 +0200

    Implement isdigit

--- includes/ft.h
+++ includes/ft.h
@@ -6,7 +6,7 @@
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/14 23:24:01 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/09 17:51:42 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/09 18:01:28 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -35,6 +35,7 @@ int		ft_strncmp(const char *s1, const char *s2, t_size n);
 int		ft_isupper(int c);
 int		ft_islower(int c);
 int		ft_isalpha(int c);
+int		ft_isdigit(int c);
 
 char	*ft_substr(char const *s, unsigned int start, t_size len);
 char	*ft_strjoin(char const *s1, char const *s2);
