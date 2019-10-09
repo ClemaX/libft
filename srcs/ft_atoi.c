d155267e73d11a6e2400d91ca50586ebf5c9a256
Clément Hamada <clementhamada@protonmail.com>
2019-10-09 18:57:37 +0200

    atoi: Fix positive sign

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/09 17:41:40 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/09 18:30:07 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/09 18:56:48 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -21,7 +21,7 @@ int	ft_atoi(const char *str)
 	while (ft_isspace(*str))
 		str++;
 	factor = *str == '-' ? -1 : 1;
-	str += *str == '-';
+	str += *str == '-' || *str == '+';
 	i = 0;
 	while (ft_isdigit(*str))
 		i = i * 10 + (*(str++) - '0');
