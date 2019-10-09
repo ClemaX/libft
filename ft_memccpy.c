1227a4a49556870858cfd5be633bd844aa8ccb52
Clément Hamada <clementhamada@protonmail.com>
2019-10-09 20:16:30 +0200

    memccpy: Fix loop

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/07 16:49:31 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/09 19:40:40 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/09 20:11:18 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,13 +16,16 @@
 void*
 	ft_memccpy(void *dst, const void *src, int c, t_size n)
 {
-	char *temp_dst;
-	char *temp_src;
+	unsigned char *temp_dst;
+	unsigned char *temp_src;
 
-	temp_dst = (char*)dst;
-	temp_src = (char*)src;
-	while (--n && *temp_src != c)
+	temp_dst = (unsigned char*)dst;
+	temp_src = (unsigned char*)src;
+	while (n && *temp_src != (unsigned char)c)
+	{
 		*(temp_dst++) = *(temp_src++);
+		n--;
+	}
 	if (n == 0)
 		return (0);
 	*(temp_dst++) = c;
