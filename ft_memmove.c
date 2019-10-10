2f93e2fa62009ac36c9aecac071fd1c0d3b6f939
Clément Hamada <clementhamada@protonmail.com>
2019-10-10 17:42:29 +0200

    memmove: Fix comparison for address overlap

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                              /             */
 /*   ft_memmove.c                                     .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/07 18:53:59 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/09 19:41:00 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/10 15:55:42 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,7 +18,7 @@ void	*ft_memmove(void *dst, const void *src, t_size len)
 	t_size i;
 
 	i = 0;
-	if (src < dst)
+	if (src > dst)
 		while (i < len)
 		{
 			((char*)dst)[i] = ((char*)src)[i];
