64c622236e18891197ca7ff33a8cb5a5ae7b09f6
Clément Hamada <clementhamada@protonmail.com>
2019-10-10 19:21:49 +0200

    strlcpy: Fix return value and null terminator

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                              /             */
 /*   ft_strlcpy.c                                     .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 22:32:49 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/09 19:42:44 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/10 19:19:19 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,11 +15,13 @@
 
 t_size	ft_strlcpy(char *dst, const char *src, t_size size)
 {
-	if (size != 0)
+	const t_size len_src = ft_strlen(src);
+
+	if (size)
 	{
-		dst[--size] = 0;
-		while (size--)
+		while (--size && *src)
 			*(dst++) = *(src++);
+		*dst = 0;
 	}
-	return (ft_strlen(src));
+	return (len_src);
 }
