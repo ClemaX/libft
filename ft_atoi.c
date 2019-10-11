5643c74116a36c79322d480032bbb5db6b00014a
Clément Hamada <clementhamada@protonmail.com>
2019-10-11 14:16:56 +0200

    atoi: Fix min/max overflows

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                              /             */
 /*   ft_atoi.c                                        .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/09 17:41:40 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/09 19:50:19 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/11 14:15:25 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,15 +15,19 @@
 
 int	ft_atoi(const char *str)
 {
-	char	factor;
-	int		i;
+	char			factor;
+	unsigned long	max;
+	unsigned long	i;
 
 	while (ft_isspace(*str))
 		str++;
 	factor = *str == '-' ? -1 : 1;
 	str += *str == '-' || *str == '+';
+	max = factor < 0 ? -9223372036854775808UL : 9223372036854775807L;
 	i = 0;
-	while (ft_isdigit(*str))
+	while (ft_isdigit(*str) && i < max)
 		i = i * 10 + (*(str++) - '0');
+	if (i >= max)
+		return (factor < 0 ? 0 : -1);
 	return (i * factor);
 }
