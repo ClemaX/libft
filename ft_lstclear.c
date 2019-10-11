8de95b73b2dc3158dcc3a72741db0adea915af48
Clément Hamada <clementhamada@protonmail.com>
2019-10-11 14:29:54 +0200

    ft_lstclear: Fix

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,16 +6,23 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/10 20:34:08 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/11 13:05:25 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/11 14:26:39 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
+
+static void	delone(t_list *lst)
+{
+	free(lst);
+	lst = NULL;
+}
 
 void		ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	ft_lstiter(*lst, del);
-	ft_lstiter(*lst, ft_lstdelone);
-	lst = 0;
+	ft_lstiter(*lst, delone);
+	lst = NULL;
 }
