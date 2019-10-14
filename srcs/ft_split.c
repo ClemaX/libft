341fd8a4aae849fb54cafacf06ac86cce4751464
Clément Hamada <clementhamada@protonmail.com>
2019-10-14 16:09:12 +0200

    ft_split: Malloc each string separately

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 17:28:54 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/10 18:56:24 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/14 16:05:37 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -33,38 +33,30 @@ static int		splitcnt(const char *s1, const char c)
 
 static t_size	splitlen(const char *s1, const char c)
 {
-	t_size	len;
+	const char	*s = s1;
 
-	len = 0;
-	while (*s1)
-	{
-		while (*s1 == c)
-			s1++;
-		if (*s1)
-			len++;
-		while (*s1 && *(s1++) != c)
-			len++;
-	}
-	return (len);
+	while (*s && *s != c)
+		s++;
+	return (s - s1);
 }
 
 char			**ft_split(char const *s, char c)
 {
-	const t_size	len_dest = splitlen(s, c);
 	char			**strs;
 	char			*dest;
 	char			**temp;
 
-	if (!len_dest)
-		return (calloc(1, 1));
-	dest = malloc(len_dest);
+	if (!s)
+		return (NULL);
 	strs = malloc(sizeof(strs) * (splitcnt(s, c) + 1));
-	if (!(dest && (temp = strs)))
+	if (!(temp = strs))
 		return (NULL);
 	while (*s)
 	{
 		while (*s == c)
 			s++;
+		if (!*s || !(dest = malloc(splitlen(s, c))))
+			return (NULL);
 		*(temp++) = dest;
 		while (*s && *s != c)
 			*(dest++) = *(s++);
