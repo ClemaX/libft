cd20e8c144673c68ea4bc5e8bd25cc316c29b132
Clément Hamada <clementhamada@protonmail.com>
2019-10-16 16:36:18 +0200

    ft_strjoin: Allow joining empty strings

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 13:02:47 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/16 13:24:04 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/16 16:35:39 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,10 +19,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len;
 	char	*s;
 
-	if (!(s1 && s2))
+	if (!(s1 || s2))
 		return (NULL);
 	len = ft_strlen(s1) + ft_strlen(s2);
-	if (!(len && (s = malloc(len + 1))))
+	if (!(s = malloc(len + 1)))
 		return (NULL);
 	while (*s1)
 		*s++ = *s1++;
