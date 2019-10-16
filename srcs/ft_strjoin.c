257eed680011eb3141fc193c04f8c9e3189e038c
Clément Hamada <clementhamada@protonmail.com>
2019-10-16 13:15:29 +0200

    ft_strjoin: Support NULL as input

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 13:02:47 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/14 15:01:02 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/16 13:15:11 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,7 +19,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len;
 	char	*s;
 
-	if (!s1 || !s2)
+	if (!(s1 || s2))
 		return (NULL);
 	len = ft_strlen(s1) + ft_strlen(s2);
 	if (!(s = malloc(len + 1)))
