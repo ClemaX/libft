f2bf77241d3d90869f3b2ef11655f6c26172b5c8
Clément Hamada <clementhamada@protonmail.com>
2019-10-16 15:21:02 +0200

    calloc: Use n_bytes

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 22:17:12 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/16 12:53:55 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/16 15:17:55 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -23,6 +23,6 @@ void	*ft_calloc(t_size count, t_size size)
 	n_bytes = count * size;
 	if (!(buf = malloc(n_bytes)))
 		return (NULL);
-	ft_bzero(buf, count * size);
+	ft_bzero(buf, n_bytes);
 	return (buf);
 }
