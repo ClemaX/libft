d754e5d1bd850eff353b6e4500cbb93cef03806a
Clément Hamada <clementhamada@protonmail.com>
2019-10-16 16:49:41 +0200

    ft_strjoin: Protect from NULL pointers

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 13:02:47 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/16 16:35:39 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/16 16:48:58 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -21,12 +21,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!(s1 || s2))
 		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2);
+	len = 0;
+	if (s1)
+		len += ft_strlen(s1);
+	if (s2)
+		len += ft_strlen(s2);
 	if (!(s = malloc(len + 1)))
 		return (NULL);
-	while (*s1)
+	while (s1 && *s1)
 		*s++ = *s1++;
-	while (*s2)
+	while (s2 && *s2)
 		*s++ = *s2++;
 	*s = 0;
 	return (s - len);
