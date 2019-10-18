cc45da15117ed60fb7b3adb26ed4f28f44275749
Clément Hamada <clementhamada@protonmail.com>
2019-10-18 17:07:36 +0200

    memchr: Cleanup

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/07 19:43:05 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/10 15:59:47 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/18 16:43:33 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,10 +15,13 @@
 
 void	*ft_memchr(const void *s, int c, t_size n)
 {
-	t_size i;
+	t_size				i;
+	unsigned char		*u_s;
+	const unsigned char	u_c = c;
 
+	u_s = (unsigned char*)s;
 	i = 0;
-	while (i < n && ((unsigned char*)s)[i] != (unsigned char)c)
+	while ((i < n) && u_s[i] != u_c)
 		i++;
-	return (i == n ? 0 : (void*)s + i);
+	return ((i == n) ? 0 : u_s + i);
 }
