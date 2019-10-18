e298187ed7391d3aac41186e2ca787524c3b0aa9
Clément Hamada <clementhamada@protonmail.com>
2019-10-18 17:07:52 +0200

    memcmp: Cleanup

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -3,10 +3,10 @@
 /*                                                              /             */
 /*   ft_memcmp.c                                      .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: chamada <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/07 20:47:48 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/09 19:40:54 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/18 16:44:47 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,10 +15,12 @@
 
 int	ft_memcmp(const void *s1, const void *s2, t_size n)
 {
-	t_size i;
+	const unsigned char	*u_s1 = s1;
+	const unsigned char	*u_s2 = s2;
+	t_size				i;
 
 	i = 0;
-	while (i < n && ((unsigned char*)s1)[i] == ((unsigned char*)s2)[i])
+	while (i < n && u_s1[i] == u_s2[i])
 		i++;
-	return (i == n ? 0 : ((unsigned char*)s1)[i] - ((unsigned char*)s2)[i]);
+	return ((i == n) ? 0 : u_s1[i] - u_s2[i]);
 }
