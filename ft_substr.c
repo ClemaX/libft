1f89bf56c48fed73a73e56b920094a804ca9141c
Clément Hamada <clementhamada@protonmail.com>
2019-10-18 12:04:07 +0200

    ft_substr: Fix overflow detection

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 12:39:22 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/16 15:15:50 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/18 12:03:08 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,7 +18,7 @@ char	*ft_substr(char const *s, unsigned int start, t_size len)
 {
 	char	*substr;
 
-	if (!(s && len + start <= ft_strlen(s) && (substr = malloc(len + 1))))
+	if (!(s && start <= ft_strlen(s) && (substr = malloc(len + 1))))
 		return (ft_calloc(1, 1));
 	s += start;
 	ft_strlcpy(substr, s, len + 1);
