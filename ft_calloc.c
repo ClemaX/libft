2e80483468a408320573df8d5faf90f611ee074e
Clément Hamada <clementhamada@protonmail.com>
2019-10-18 17:06:45 +0200

    calloc: Cleanup

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 22:17:12 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/16 15:17:55 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/18 16:29:11 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,10 +17,9 @@
 
 void	*ft_calloc(t_size count, t_size size)
 {
-	void	*buf;
-	t_size	n_bytes;
+	void			*buf;
+	const t_size	n_bytes = count * size;
 
-	n_bytes = count * size;
 	if (!(buf = malloc(n_bytes)))
 		return (NULL);
 	ft_bzero(buf, n_bytes);
