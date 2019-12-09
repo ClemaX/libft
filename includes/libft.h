be44f3508597f745c4ccc5415f94a1257b7e6e30
Clément Hamada <clementhamada@protonmail.com>
2019-12-09 17:07:44 +0100

    Update header

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/14 23:24:01 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2019/12/09 17:05:17 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/12/09 17:07:21 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -60,8 +60,8 @@ char					*ft_strjoin(char const *s1, char const *s2);
 char					*ft_strtrim(char const *s1, char const *set);
 char					**ft_split(char const *s, char c);
 char					*ft_itoa(int n);
-char					*ft_strmapi(char const *s,
-	char (*f)(unsigned int, char));
+char					*ft_strmapi
+	(char const *s, char (*f)(unsigned int, char));
 void					ft_putchar_fd(char c, int fd);
 void					ft_putstr_fd(char *s, int fd);
 void					ft_putendl_fd(char *s, int fd);
