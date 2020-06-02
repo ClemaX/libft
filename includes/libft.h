abe7113cb7acdc984ae7a7d15ae44ba77ece4c54
Clément Hamada <clementhamada@protonmail.com>
2020-06-02 14:41:55 +0200

    Add ft_putchar

--- includes/libft.h
+++ includes/libft.h
@@ -72,6 +72,7 @@ char			*ft_strmapi
 	(char const *s, char (*f)(unsigned int, char));
 void			ft_striter(char *str, void (*fun)(char));
 void			ft_putchar_fd(char c, int fd);
+int				ft_putchar(int c);
 void			ft_putstr_fd(char *s, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
--- srcs/io/ft_putchar_fd.c
+++ srcs/io/ft_putchar_fd.c
@@ -17,3 +17,8 @@ void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
+
+int		ft_putchar(int c)
+{
+	return (write(1, &c, 1));
+}
