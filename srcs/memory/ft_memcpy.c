a76485e361d9c2c01601623dc1033a531d10ca4e
Clément Hamada <chamada@student.le-101.fr>
2020-11-12 10:50:46 +0100

    memcpy: Fix dereference of NULL pointer

--- srcs/memory/ft_memcpy.c
+++ srcs/memory/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 16:03:24 by chamada           #+#    #+#             */
-/*   Updated: 2020/08/23 16:31:13 by chamada          ###   ########.fr       */
+/*   Updated: 2020/11/12 10:50:22 by chamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	char *temp;
 
-	if (!dst && !src)
-		return (0);
+	if (!dst || !src)
+		return (NULL);
 	temp = dst;
 	while (n--)
 		*temp++ = *(char *)src++;
