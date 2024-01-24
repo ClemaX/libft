53a32e7ece4d00b192f98f44770bcc331a14539d
Clement Hamada <clementhamada@protonmai.com>
2024-01-24 14:14:30 +0100

    test: path_cat: Add more complex tests

--- test/src/units/paths/path_cat.unit.c
+++ test/src/units/paths/path_cat.unit.c
@@ -22,10 +22,16 @@ static int	run(const struct args *args)
 const unit	unit_paths_path_cat = {
 	"path_cat",
 	(test[]){
-		//												a		b		expected
-		it("should concatenate relative paths", 		"a",	"b",	"a/b"),
-		it("should concatenate . on relative path",		"a",	".",	"a"),
-		it("should concatenate .. on relative path",	"a/b",	"..",	"a"),
+		//												a			b			expected
+		it("should cat relative paths", 				"a",		"b",		"a/b"),
+		it("should cat . on relative path",				"a",		".",		"a"),
+		it("should cat .. on relative path",			"a/b",		"..",		"a"),
+		it("should cat multiple . on relative path",	"a",		"././.",	"a"),
+		it("should handle empty input as a path",		"a",		"",			"a"),
+		it("should handle empty input as b path",		"",			"b",		"b"),
+		it("should handle absolute paths",				"/a",		"/b",		"/a/b"),
+		it("should handle mixed paths",					"/a",		"b",		"/a/b"),
+		it("should handle complex relative paths",		"a/b/c",	"../../d",	"a/d"),
 		{},
 	},
 };
