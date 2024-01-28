b4dcaf599d8d36a7307bd11ab18ac4d3ff0ac139
Clément Hamada <clementhamada@protonmail.com>
2024-01-28 23:03:08 +0100

    strings: strtok: Refactor and fix edge cases

--- src/strings/ft_strtok.c
+++ src/strings/ft_strtok.c
@@ -1,44 +1,41 @@
 #include <libft/strings.h>
+#include <stddef.h>
 
-static size_t	token_next(char *restrict *next, char *restrict *token, const char *delim)
+static inline char	*delims_skip(char *restrict str, const char *restrict delim, size_t delim_len)
 {
-	size_t	end_i;
+	size_t	i;
+
+	i = 0;
+	while (str[i] == delim[i % delim_len])
+		++i;
+	if (i % delim_len != 0)
+		i -= i % delim_len;
+	return (str + i);
+}
+
+static inline char	*delim_find(char *restrict str, const char *restrict delim)
+{
 	size_t	delim_i;
 
-	end_i = 0;
-	delim_i = 0;
 	do
 	{
 		delim_i = 0;
-		while (delim[delim_i] != '\0' && (*next)[end_i] == delim[delim_i])
-		{
-			end_i++;
-			delim_i++;
-		}
+		while (*str != '\0' && *str != delim[0])
+			++str;
+		while (str[delim_i] == delim[delim_i])
+			++delim_i;
 		if (delim[delim_i] != '\0')
-			end_i++;
+			str += delim_i;
 	}
-	while (delim[delim_i] != '\0' && (*next)[end_i] != '\0');
-
-	if (delim[delim_i] == '\0' || (*next)[end_i] == '\0')
-	{
-		*token = *next;
-		(*next) += end_i;
-
-		if (end_i == delim_i)
-		{
-			end_i = 0;
-			delim_i = 0;
-		}
-	}
-	return (delim_i);
+	while (*str!= '\0' && delim[delim_i] != '\0');
+	return (str);
 }
 
-char	*ft_strtok(char *restrict str, const char *restrict delim)
+
+char			*ft_strtok(char *restrict str, const char *restrict delim)
 {
 	static char *restrict	start;
-	size_t					delim_i;
-
+	size_t					delim_len;
 
 	if (str != NULL)
 		start = str;
@@ -46,12 +43,16 @@ char	*ft_strtok(char *restrict str, const char *restrict delim)
 		str = start;
 	if (*delim != '\0' && *start != '\0')
 	{
-		delim_i = token_next(&start, &str, delim);
-		if (delim[delim_i] == '\0')
-			start[-delim_i] = '\0';
+		delim_len = ft_strlen(delim);
+		str = delims_skip(str, delim, delim_len);
+		start = delim_find(str, delim);
+		if (*start != '\0')
+		{
+			*start = '\0';
+			start += delim_len;
+		}
 	}
 	if (str != NULL && *str == '\0')
 		str = NULL;
-
 	return (str);
 }
